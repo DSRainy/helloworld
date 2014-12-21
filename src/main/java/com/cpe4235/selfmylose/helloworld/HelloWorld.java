ac6dca6034c663352d19b1488c39226174b2fd55
RainWhileLoop <RainWhileLoop@Rainy-PC>
2014-12-21 09:07:22 +0700

    First Commit

---
+++ src/main/java/com/cpe4235/selfmylose/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cpe4235.selfmylose.helloworld;
+
+/**
+ *
+ * @author RainWhileLoop
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
